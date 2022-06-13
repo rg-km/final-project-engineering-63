85fdbbb0f73d73458db21bfc8bc1f160461d2ce5
ArgaPahreja <argapahreja48@gmail.com>
2022-06-13 11:40:58 +0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./App.css"
+
+const App = () => {
+  return (
+    <>
+     <h1>
+      Hello World
+     </h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/pages/HomePages.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Home from "../home/homes/Home"
+import About from "../home/about/About"
+import Cards from "../home/cards/Cards"
+
+
+const HomePages = () => {
+  return (
+    <>
+        <Home />
+        <About />
+        <Cards />
+    </>
+  )
+}
+
+export default HomePages
