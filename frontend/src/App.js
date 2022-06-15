02afcb430dbe4b5480cb05005f35d643f61f55fd
rymzx <rmasihin7@gmail.com>
2022-06-15 09:32:35 +0700

    adding css on register pages, no img

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Register from './components/login/Register';
+import './components/pages/RegisterPages.css';
+// import Login from './components./login/Login';
+// import './components/pages/LoginPages.css';
 
 const App = () => {
   return (
     <>
       <h1>
+        {/* <Login /> */}
         <Register />
       </h1>
     </>
--- frontend/src/components/pages/RegisterPages.jsx
+++ frontend/src/components/pages/RegisterPages.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Login from '../login/Login';
-import GreatingsImage from '../public/assets/greating-image.png';
+import GreatingsImage from '../../../public/assets/greating-image.png';
 
 const RegisterPages = () => {
   return (
     <>
       <Login />
-      <div className="register-image">
+      <div className="login-image">
         <img src={GreatingsImage} alt="" height={400} width={400} />
       </div>
     </>
