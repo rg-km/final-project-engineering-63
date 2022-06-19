d67760952a2e0f640e885d62c02d282739aef95b
StevenAryaDijaya <dijayastevenarya@gmail.com>
2022-06-19 15:51:49 +0700

    fix overlaysoal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
 import React from "react"
 import "./App.css"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-// import Footer from "./components/home/Footer"
-// import Header from "./components/home/header/Header"
-// import HomePages from "./components/pages/HomePages"
-// import LoginPages from "./components/pages/LoginPages"
+import Footer from "./components/home/Footer"
+import Header from "./components/home/header/Header"
+import HomePages from "./components/pages/HomePages"
+import LoginPages from "./components/pages/LoginPages"
 import CoursePages from "./components/pages/CoursePages"
 
 const App = () => { 
   return (
     <>
-      {/* <Router>
+      <Router>
         <Header />
-        <Switch> */}
-          {/* <Route path='/' exact component={HomePages} />
-          <Route path='/login' exact component={LoginPages} /> */}
-          {/* <Route path='/course' exact component={CoursePages} />
+        <Switch>
+          <Route path='/' exact component={HomePages} />
+          <Route path='/login' exact component={LoginPages} /> 
+          <Route path='/course' exact component={CoursePages} />
         </Switch>
         <Footer />
-      </Router> */}
-      <CoursePages />
+      </Router>
     </>
   )
 }
