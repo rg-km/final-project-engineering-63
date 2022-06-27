366b27492c883a457e5affd18b7a4ea440098310
StevenAryaDijaya <dijayastevenarya@gmail.com>
2022-06-27 23:16:35 +0700

    fix result

--- frontend/src/components/Result/result.jsx
+++ frontend/src/components/Result/result.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import TestResult from "./ResultTest"
+import TestResult from "./resultTest"
 import "./style.css"
 import { Link } from "react-router-dom"
 
--- frontend/src/components/pages/ResultPages.jsx
+++ frontend/src/components/pages/ResultPages.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Result from "../Result/Result"
+import Result from "../Result/result"
 
 const ResultPages = () => {
 
